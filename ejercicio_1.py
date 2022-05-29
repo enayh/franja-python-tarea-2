90b20309ca1ac0f94d916d8cf5dd2b050dd89e12
enayh <n_perez@alumnos.upla.cl>
2022-05-28 22:23:17 -0400

    Update

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,7 +1,6 @@
 # Escribir un programa en el que se pregunte al usuario por una frase y una letra, y muestre por pantalla el número de veces que aparece la letra en la frase.
 def buscador(frase, letra):
     cantidad = 0
-
     for contador in range(len(frase)):
         letra_aux = frase[contador]
         if letra_aux == letra.lower() or letra_aux == letra.upper():
@@ -14,10 +13,10 @@ def main():
     
     cantidad = buscador(frase, letra)
     
-    if cantidad < 0:
-        print("La letra "+letra+" aparece "+str(cantidad)+" veces en la frase.")
+    if cantidad > 0:
+        print("La letra '"+letra+"' aparece "+str(cantidad)+" veces en la frase.")
     else:
-        print("La letra "+letra+" no aparece en la frase.")
+        print("La letra '"+letra+"' no aparece en la frase.")
 
 if __name__ == '__main__':
     main()
